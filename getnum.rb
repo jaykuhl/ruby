3e72b6dc96be849b2fa573211ed8a3e3178701df
jaykuhl <jkuhlar@me.com>
2018-07-17 21:06:32 +1000

    Who dis test

--- getnum.rb
+++ getnum.rb
@@ -13,3 +13,5 @@ puts "Get money"
 # When I wrote code I only want to go so far because some people live in the 80s
 # and thats messed up, so only go to the 80 character limited because going any
 # further is messed up.
+
+#who dis?

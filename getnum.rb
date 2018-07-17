8e07858d7ea385e2465233cdb2e86220e943d6dd
jaykuhl <jkuhlar@me.com>
2018-07-17 20:54:36 +1000

    asd

--- getnum.rb
+++ getnum.rb
@@ -11,5 +11,5 @@ puts "$50 and it is yours"
 puts "Get money"
 
 # When I wrote code I only want to go so far because some people live in the 80s
-# and thats messed up, so only go to the 80 character limite because going any
+# and thats messed up, so only go to the 80 character limited because going any
 # further is messed up.

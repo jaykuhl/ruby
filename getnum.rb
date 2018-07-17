2d792e05941fecdc06842c8ba40e7c732996f2c5
jaykuhl <jkuhlar@me.com>
2018-07-17 20:52:39 +1000

    comment

--- getnum.rb
+++ getnum.rb
@@ -9,3 +9,7 @@ puts "yolo"
 puts "$50 and it is yours"
 
 puts "Get money"
+
+# When I wrote code I only want to go so far because some people live in the 80s
+# and thats messed up, so only go to the 80 character limite because going any
+# further is messed up.
